fb64027c0a974efe81121d14f71586b356305b5b
doidin123 <igormedeiros1999@gmail.com>
2021-06-25 19:04:33 -0300

    Criação de funções para salvar os player em games

--- src/com/company/objects/Game.java
+++ src/com/company/objects/Game.java
@@ -28,28 +28,25 @@ public class Game {
         return Players;
     }
 
+
     public void Kills(Player p) {
         //aumenta o total de kills e adiciona o player
         this.totalKills = this.totalKills + 1;
-        if (Players.contains(p)) {
-            int index = Players.indexOf(p);
-            int kill = Players.get(index).getKills() + 1;
-            Players.get(index).setKills(kill);
-        } else {
-            p.setKills(1);
-            Players.add(p);
-        }
-
+        int index = Players.indexOf(p);
+        int kill = Players.get(index).getKills() + 1;
+        Players.get(index).setKills(kill);
     }
-    public void worldKills(){
 
+    public void worldKills() {
+        //aumenta a quantidade de kills total e diminui em 1 do jogador que morreu
     }
+
 
     @Override
     public String toString() {
-        return "Game" + "_" + gameNumber + "{" +
-                "totalKills:" + totalKills + ";" +
-                "/n, Players:" + Players +
-                '}';
+        return "Game" + "_" + gameNumber + ": {\n" +
+                "totalKills: " + totalKills + ";" +
+                "\nPlayers e kills: " + Players +
+                "\n}";
     }
 }
--- src/com/company/parser/Parser.java
+++ src/com/company/parser/Parser.java
@@ -9,36 +9,72 @@ import java.io.BufferedReader;
 
 public class Parser {
     public Parser(String log) throws IOException {
-        int gameCount = 1;
+        int gameCount = 0;
         BufferedReader br = new BufferedReader(new FileReader(log));
         for (String line; (line = br.readLine()) != null; ) {
             //leitura linha por linha
 
             if (line.contains("InitGame:")) {
                 // o jogo inicia
+                gameCount = gameCount + 1;
                 Game g = new Game(gameCount);
                 do {
-                    if (line.contains("Kill:")) {
+                    if (line.contains("ClientUserinfoChanged:")) {
+                        //alguem se conectou
+                        g = playerSave(line, g);
+
+                    }
+
+                   /* if (line.contains("Kill:")) {
                         //encontrou uma kill
                         if (line.contains("<world>")) {
                             //kill do "<world>"
                         } else {
                             //kill de player
                             Player aux = new Player(Killer(line));
+                            g.Kills(aux);
                         }
 
-                    }
+                    }*/
                     line = br.readLine();
                 } while (!(line.contains("ShutdownGame:")));
                 //termina quando encontrar "ShutdownGame:"
+                System.out.println(g);
+                System.out.println("");
             }
-            gameCount = gameCount + 1;
+
         }
+    }
+
+    private Game playerSave(String line, Game g) {
+        //adiciona players ao game
+        line = line.substring(line.indexOf("n\\") + 2, line.indexOf("\\t"));
+        Player aux = new Player(line);
+        if (!g.getPlayers().contains(aux)) {
+            //testa se o player ja existe e não adiciona de novo
+            g.getPlayers().add(aux);
+        }
+        System.out.println(aux);
+        return g;
     }
 
     private String Killer(String line) {
         //encontrar o nome do "killer"
-        String aux = line.substring(line.indexOf("Kill:"), line.indexOf("killed"));
+        String aux = line.substring(line.indexOf("Kill:"));
+        String[] parts = aux.split(":");
+        aux = parts[parts.length - 1].trim();
+       /* for (int i = 0; i < aux.length(); i = i + 1) {
+            String aux2;
+            if(aux.charAt(i)=" "&&aux.)
+        }*/
+
+
+        return aux;
+    }
+
+    private String worldKill(String line) {
+        //encontrar o nome do jogador morto
+        String aux = line.substring(line.indexOf("killed"));
         String[] parts = aux.split(":");
         aux = parts[parts.length - 1].substring(1);
         return aux;
